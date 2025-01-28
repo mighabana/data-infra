d4b503940713061dd78d2240200b00fccb981f75
Miguel Habana <mighabana@gmail.com>
2025-01-28 21:51:55 +0800

    Update version requirements to be more flexible

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
 terraform {
-  required_version = "~> 1.9.5"
+  required_version = ">= 1.9.5"
 
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "~> 6.2.0"
     }
     google-beta = {
       source = "hashicorp/google-beta"
-      version = "~>4"
     }
   }
 
