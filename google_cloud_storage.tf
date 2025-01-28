b804921a70eb7a931106a9bc8983cc564209f261
Miguel Habana <mighabana@gmail.com>
2025-01-28 21:53:19 +0800

    Fix issue with uploading google takeout files to GCS

--- google_cloud_storage.tf
+++ google_cloud_storage.tf
@@ -1,4 +1,3 @@
-# TODO: parameterize locations for all GCS buckets
 # would be good to find a way to track the cheapest locations (Europe, Asia, Americas)
 
 resource "google_storage_bucket" "compendium-test" {
@@ -44,22 +43,36 @@ resource "google_storage_bucket" "data-seeds" {
   public_access_prevention = "enforced"
 }
 
-resource "google_storage_bucket_object" "google_takeout" {
-  for_each  = toset(var.google_takeout_source_list)
-  
-  name  = join("", ["google/takeout/", join("/", regex("(\\d{8}T\\d{6}Z)-(\\d{3}.zip)", each.key))])
-  source = each.key
-  bucket  = "data-seeds"
+# use gsutil to upload the file since takeout files are huge
+resource "null_resource" "upload_takeout_files" {
+  provisioner "local-exec" {
+    command = "gsutil cp ${each.value} gs://${google_storage_bucket.data-seeds.name}/google/takeout/${join("/", regex("(\\d{8}T\\d{6}Z)-(\\d{3}.zip)", each.value))}"
+  }
 
-  content_type = "application/zip"
+  for_each = toset(var.google_takeout_source_list)
 }
+
+# NOTE: This was the code block I used before but it errors now
+# I don't remember if it was an issue before as well but it is
+# it is most likely due to the file being too large
+#
+# resource "google_storage_bucket_object" "google_takeout" {
+#   for_each  = toset(var.google_takeout_source_list)
+  
+#   name  = join("", ["google/takeout/", join("/", regex("(\\d{8}T\\d{6}Z)-(\\d{3}.zip)", each.value))])
+#   bucket  = google_storage_bucket.data-seeds.name
+
+#   content_type = "application/zip"
+
+#   depends_on = [null_resource.upload_takeout_files]
+# }
 
 resource "google_storage_bucket_object" "spotify" {
   for_each = toset(var.spotify_source_list)
 
-  name = join("", ["spotify/", join("/", regex("([^\\/]\\w+)-(\\d{8}T\\d{6}.zip)", each.key))])
+  name = join("", ["spotify/", join("/", regex("([^\\/-]\\w+)-(\\d{8}T\\d{6}.zip)", each.key))])
   source = each.key
-  bucket = "data-seeds"
+  bucket = google_storage_bucket.data-seeds.name
 
   content_type = "application/zip"
 }
