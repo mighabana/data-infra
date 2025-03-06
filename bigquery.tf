1cec4ce5b9a814e3e07f79e81feef77fdec37f7a
Miguel Habana <mighabana@gmail.com>
2025-03-06 14:30:30 +0100

    Remove hard-coded locations on bigquery datasets

--- bigquery.tf
+++ bigquery.tf
@@ -1,7 +1,7 @@
 resource "google_bigquery_dataset" "bronze_spotify" {
-  dataset_id    = "bronze_spotify"
-  description   = "Raw data extracted from Spotify"
-  location      = "europe-west4"
+  dataset_id  = "bronze_spotify"
+  description = "Raw data extracted from Spotify"
+  location    = var.region
 
   labels = {
     env = "default"
@@ -19,9 +19,9 @@ resource "google_bigquery_dataset" "bronze_spotify" {
 }
 
 resource "google_bigquery_dataset" "bronze_google" {
-  dataset_id    = "bronze_google"
-  description   = "Raw data extracted from Google"
-  location      = "europe-west4"
+  dataset_id  = "bronze_google"
+  description = "Raw data extracted from Google"
+  location    = var.region
 
   labels = {
     env = "default"
@@ -39,9 +39,9 @@ resource "google_bigquery_dataset" "bronze_google" {
 }
 
 resource "google_bigquery_dataset" "bronze_open_alex" {
-  dataset_id = "bronze_open_alex"
+  dataset_id  = "bronze_open_alex"
   description = "Raw data extracted from OpenAlex"
-  location = "europe-west4"
+  location    = var.region
 
   labels = {
     env = "default"
